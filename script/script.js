f57f389dddb25f8250f37ca545b9d887ba003c91
Juliana Ferreira <juliana.sousa18@hotmail.com>
2022-09-05 19:37:15 -0300

    criado função que calcula imc

--- script/script.js
+++ script/script.js
@@ -1,13 +1,21 @@
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 
 function handleForm(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     //valores dos inputs
-    let peso = document.getElementById("peso").value
-    let altura = document.getElementById("altura").value
-    let faixaEtaria = document.getElementById("faixa-etaria").value
-
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    let faixaEtaria = document.getElementById("faixa-etaria").value;
+    
 }
 
-form.addEventListener("submit", handleForm)
+form.addEventListener("submit", handleForm);
+
+function calculaImc() {
+        let alturaAoQuadrado = altura.value * altura.value;
+        let imc = peso.value / alturaAoQuadrado;
+        let resultado = document.getElementById("response");
+        resultado.innerHTML = imc
+        return imc
+}
