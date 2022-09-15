93fd7117532e56c69c2e2a0a2dacc19d2dc3ef7d
Flaviano Rodrigues <flavianodev@gmail.com>
2022-09-15 12:28:35 -0300

    Alterando mensagens de feedback do IMC

--- script/script.js
+++ script/script.js
@@ -27,25 +27,25 @@ function handleForm(/*e*/) {
   if (faixaEtaria === 'adulto') {
     if (imc < 18.5) {
       mensagem = 'Está abaixo do peso recomendado.'
-    } else if (imc >= 18.5 && imc <= 24.9) {
+    } else if (imc <= 24.9) {
       mensagem = 'Peso dentro da faixa recomendada.'
-    } else if (imc >= 25.0 && imc <= 29.9) {
-      mensagem = 'Ta bem tratado.'
-    } else if (imc >= 30.0 && imc <= 34.9) {
-      mensagem = 'Muito bem tratado.'
-    } else if (imc >= 35.0 && imc <= 39.9) {
-      mensagem = 'Muito, Muito bem tratado'
+    } else if (imc <= 29.9) {
+      mensagem = 'Excesso de peso.'
+    } else if (imc <= 34.9) {
+      mensagem = 'Obesidade de Classe 1.'
+    } else if (imc <= 39.9) {
+      mensagem = 'Obesidade de Classe 2.'
     } else {
-      mensagem = 'Aprecia tudo que tem vontade.'
+      mensagem = 'Obesidade de Classe 3.'
     }
     // Esse bloco irá calcular o peso de pessoas "Idosas"
   } else if (faixaEtaria === 'idoso') {
     if (imc <= 22) {
       mensagem = 'Está abaixo do peso recomendado.'
-    } else if (imc > 22 && imc < 27) {
+    } else if (imc < 27) {
       mensagem = 'Peso dentro da faixa recomendada.'
     } else {
-      mensagem = 'Ta bem tratado, continue assim...'
+      mensagem = 'Sobrepeso'
     }
   } else alert('Por favor selecione se Adulto ou Idoso. Grato!')
 
